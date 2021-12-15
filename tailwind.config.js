207330f149d996715b290268ed6018514b61835c
JustSampei <graziano@GrazianoArch>
2021-12-15 22:00:45 +0100

    feat: added tailwind, feat: added some more assets

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ["./src/*.{html,js}"],
+    theme: {
+        colors: {
+            terracotta: "#a06056",
+            "brown-light": "#706458",
+            "brown-dark": "#534b42",
+            black: "#121212",
+            white: "#fff",
+            "terracotta-light": "#f3eee8",
+        },
+        fontFamily: {
+            merri: "merriweather",
+            varta: "varta",
+        },
+    },
+    plugins: [],
+};
