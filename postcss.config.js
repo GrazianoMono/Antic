c5c251dfcd668acfbeb7d355f666fdf531b21ae0
JustSampei <graziano@GrazianoArch>
2021-12-19 21:11:04 +0100

    fix: converted nesting to tailwind nesting and added postcss import

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,11 @@
 module.exports = {
     plugins: [
-        require("postcss-nested"),
+        require("postcss-import"),
         require("autoprefixer"),
         require("cssnano")({
             preset: "default",
         }),
+        require("tailwindcss/nesting"),
         require("tailwindcss"),
     ],
 };
